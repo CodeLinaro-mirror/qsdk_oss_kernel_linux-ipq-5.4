33b7c2250e0c5c6a89eebdb89f387b84a30b5481
Imran Khan <imran.f.khan@oracle.com>
2022-08-15 05:53:53 +1000

    kfence: add sysfs interface to disable kfence for selected slabs.
    By default kfence allocation can happen for any slab object, whose size is
    up to PAGE_SIZE, as long as that allocation is the first allocation after
    expiration of kfence sample interval.  But in certain debugging scenarios
    we may be interested in debugging corruptions involving some specific slub
    objects like dentry or ext4_* etc.  In such cases limiting kfence for
    allocations involving only specific slub objects will increase the
    probablity of catching the issue since kfence pool will not be consumed by
    other slab objects.
    
    This patch introduces a sysfs interface
    '/sys/kernel/slab/<name>/skip_kfence' to disable kfence for specific
    slabs.  Having the interface work in this way does not impact
    current/default behavior of kfence and allows us to use kfence for
    specific slabs (when needed) as well.  The decision to skip/use kfence is
    taken depending on whether kmem_cache.flags has (newly introduced)
    SLAB_SKIP_KFENCE flag set or not.
    
    Change-Id: I72105727775e855a57b7cb3aa472824cf61ce98c
    Link: https://lkml.kernel.org/r/20220814195353.2540848-1-imran.f.khan@oracle.com
    Signed-off-by: Imran Khan <imran.f.khan@oracle.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Marco Elver <elver@google.com>
    Reviewed-by: Hyeonggon Yoo <42.hyeyoo@gmail.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Git-commit: b84e04f1baeebe6872b22a027cfc558621e842d4
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Sridharan S N <sridsn@qti.qualcomm.com>

--- mm/kfence/core.c
+++ mm/kfence/core.c
@@ -941,6 +941,13 @@ void *__kfence_alloc(struct kmem_cache *s, size_t size, gfp_t flags)
 		return NULL;
 	}
 
+	/*
+	 * Skip allocations for this slab, if KFENCE has been disabled for
+	 * this slab.
+	 */
+	if (s->flags & SLAB_SKIP_KFENCE)
+		return NULL;
+
 	allocation_gate = atomic_inc_return(&kfence_allocation_gate);
 	if (allocation_gate > 1)
 		return NULL;
