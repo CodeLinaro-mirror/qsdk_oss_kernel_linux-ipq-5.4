ed5a266bc5f7db92475f9cf47704ec18031a2209
Ronghua Zeng <quic_ronghuaz@quicinc.com>
2023-04-18 15:25:35 +0800

    MIPS: ath79: ath9k exports
    This was copied from https://git.codelinaro.org/clo/qsdk/oss/system/openwrt_repo/-/blob/90daff4cf8a86622012217d3be7ce136c313a896/target/linux/ath79/patches-5.4/0034-MIPS-ath79-ath9k-exports.patch
    Change-Id: Id227155e9c901efebb2507c65f7ebde231a5b1cb
    Signed-off-by: Ronghua Zeng <quic_ronghuaz@quicinc.com>

--- arch/mips/ath79/common.c
+++ arch/mips/ath79/common.c
@@ -36,7 +36,8 @@ EXPORT_SYMBOL_GPL(ath79_soc_rev);
 void __iomem *ath79_pll_base;
 void __iomem *ath79_reset_base;
 EXPORT_SYMBOL_GPL(ath79_reset_base);
-static void __iomem *ath79_ddr_base;
+void __iomem *ath79_ddr_base;
+EXPORT_SYMBOL_GPL(ath79_ddr_base);
 static void __iomem *ath79_ddr_wb_flush_base;
 static void __iomem *ath79_ddr_pci_win_base;
 
