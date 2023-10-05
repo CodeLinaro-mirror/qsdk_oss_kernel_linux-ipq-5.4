bf4a3f7e5dbfdd4d86a4fafb6f74fea9befab422
Nandha Kishore Easwaran <quic_nandhaki@quicinc.com>
2023-10-05 12:57:52 +0530

    net: skbuff: Clear skb flags during skb copy
    Clear fast_xmit and other recycler related flags during
    skb_copy.
    
    Change-Id: I80f355a8ff024ae1daa12d7d7113e9bdd485030e
    Signed-off-by: Nandha Kishore Easwaran <quic_nandhaki@quicinc.com>
    (cherry picked from commit 0a60a92dfa0ffec73acbaada55ef1c48dd24b5a2)

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -1234,6 +1234,14 @@ static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 	memcpy(&new->headers_start, &old->headers_start,
 	       offsetof(struct sk_buff, headers_end) -
 	       offsetof(struct sk_buff, headers_start));
+
+	/* Clear the skb recycler flags here to make sure any skb whose size
+	 * has been altered is not put back into recycler pool.
+	 */
+	new->fast_xmit = 0;
+	new->is_from_recycler = 0;
+	new->fast_recycled = 0;
+	new->recycled_for_ds = 0;
 	CHECK_SKB_FIELD(protocol);
 	CHECK_SKB_FIELD(csum);
 	CHECK_SKB_FIELD(hash);
@@ -1261,7 +1269,6 @@ static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 #ifdef CONFIG_NET_SCHED
 	CHECK_SKB_FIELD(tc_index);
 #endif
-
 }
 
 /*
@@ -1977,6 +1984,14 @@ int pskb_expand_head(struct sk_buff *skb, int nhead, int ntail,
 	 */
 	if (!skb->sk || skb->destructor == sock_edemux)
 		skb->truesize += size - osize;
+
+	/* Clear the skb recycler flags here to make sure any skb whose size
+	 * has been expanded is not put back into recycler.
+	 */
+	skb->fast_xmit = 0;
+	skb->is_from_recycler = 0;
+	skb->fast_recycled = 0;
+	skb->recycled_for_ds = 0;
 
 	return 0;
 
