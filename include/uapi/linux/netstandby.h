d06818d7da7f9df98264e64a13a0c446e0fb285f
syed touqeer pasha <quic_spasha@quicinc.com>
2024-01-18 17:54:42 +0530

    netstandby: Add support for ethernet aware wifi tx power
    Add support for ethernet aware wifi tx power
    
    Change-Id: I8b15b85aace908cde25fee49dca5a7c42c425805
    Signed-off-by: syed touqeer pasha <quic_spasha@quicinc.com>
    (cherry picked from commit b3dccdce96b232514c00108d9442a606267637e3)

--- include/uapi/linux/netstandby.h
+++ include/uapi/linux/netstandby.h
@@ -97,12 +97,36 @@ struct netstandby_entry_info {
 };
 
 /*
+ * netstandby_eawtp_port_info
+ */
+struct netstandby_eawtp_port_info {
+	uint8_t num_active_port;	/**< Number of active ports */
+};
+
+/*
  * Callbacks type
  */
 typedef int (*netstandby_exit_cb_t) (void *app_data, struct netstandby_exit_info *exit_info);
 typedef int (*netstandby_enter_cb_t) (void *app_data, struct netstandby_entry_info *enter_info);
 typedef void (*netstandby_event_compl_cb_t) (void *app_data, struct netstandby_event_compl_info *event_info);
 typedef void (*netstandby_trigger_notif_cb_t) (void *app_data, struct netstandby_trigger_info *trigger_info);
+typedef int (*netstandby_eawtp_get_num_active_ports_cb_t) (void *app_data, struct netstandby_eawtp_port_info *pinfo);
+typedef int (*netstandby_eawtp_notify_active_ports_cb_t) (void *app_data, struct netstandby_eawtp_port_info *ninfo);
+typedef int (*netstandby_eawtp_port_link_notify_register_cb_t) (void *app_data);
+
+/*
+ * struct netstandby_eawtp_reg_info
+ * 	Eawtp registration structure
+ * TODO: Cleanup using new header file.
+ */
+struct netstandby_eawtp_reg_info {
+	netstandby_eawtp_get_num_active_ports_cb_t get_active_ports_cb;	/**< Get number of active ports */
+	netstandby_eawtp_notify_active_ports_cb_t ntfy_active_ports_cb;	/**< Notify active ports after any port link state change */
+	netstandby_eawtp_notify_active_ports_cb_t ntfy_port_status_to_wifi_cb;	/**< Notify active ports status to wifi */
+	netstandby_eawtp_get_num_active_ports_cb_t get_nss_active_port_cnt_cb; /**< Get number of active ports from DP */
+	netstandby_eawtp_port_link_notify_register_cb_t port_link_notify_register_cb;/**< Port link notifier register */
+	netstandby_eawtp_port_link_notify_register_cb_t port_link_notify_unregister_cb;/**< Port link notifier unregister */
+};
 
 /*
  * struct netstandby_reg_info
@@ -125,4 +149,8 @@ struct netstandby_reg_info {
 int nss_dp_get_and_register_cb(struct netstandby_reg_info *info);
 int netstandby_wifi_get_and_register_cb(struct netstandby_reg_info *info);
 int netstandby_platform_get_and_register_cb(struct netstandby_reg_info *info);
+int netstandby_eawtp_nss_get_and_register_cb(struct netstandby_eawtp_reg_info *info);
+int netstandby_eawtp_wifi_get_and_register_cb(struct netstandby_eawtp_reg_info *info);
+int netstandby_eawtp_nss_unregister_cb(void);
+int netstandby_eawtp_wifi_unregister_cb(void);
 #endif
