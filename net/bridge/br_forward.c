21d210676d1ebcddb159fd71c926aced5b615547
syed touqeer pasha <quic_spasha@quicinc.com>
2024-02-12 20:33:44 +0530

    Revert "arm/arm64: enable hairpin for wlan interfaces for multicast traffic"
    This reverts commit 98c1612dd31226d04879717ad343b34340b16ab8.
    
    Change-Id: Ic2bdc7a25453fcb27d6158336a75ef185360d180
    Signed-off-by: syed touqeer pasha <quic_spasha@quicinc.com>

--- net/bridge/br_forward.c
+++ net/bridge/br_forward.c
@@ -24,19 +24,11 @@ static inline int should_deliver(const struct net_bridge_port *p,
 	struct net_bridge_vlan_group *vg;
 
 	vg = nbp_vlan_group_rcu(p);
-	if  (((p->flags & BR_HAIRPIN_MODE) || (skb->dev != p->dev)) &&
+	return (((p->flags & BR_HAIRPIN_MODE) && !is_multicast_ether_addr(eth_hdr(skb)->h_dest))
+		|| (skb->dev != p->dev)) &&
 		br_allowed_egress(vg, skb) && (p->state == BR_STATE_FORWARDING) &&
 		nbp_switchdev_allowed_egress(p, skb) &&
-		!br_skb_isolated(p, skb)) {
-		if (!is_multicast_ether_addr(eth_hdr(skb)->h_dest)) {
-			return true;
-		}
-
-		if (skb->dev->ieee80211_ptr) {
-			return true;
-		}
-	}
-	return false;
+		!br_skb_isolated(p, skb);
 }
 
 int br_dev_queue_push_xmit(struct net *net, struct sock *sk, struct sk_buff *skb)
