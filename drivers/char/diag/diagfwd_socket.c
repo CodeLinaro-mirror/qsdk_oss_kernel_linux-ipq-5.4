2792a2e09abb7c5e7b21c1f2240f2f8c45ac8335
Amandeep Singh <quic_amansing@quicinc.com>
2024-04-03 16:33:01 +0530

    diag: Add mask to enable and disable diag servers
    Add peripheral mask to enable and disable diag servers from
    module param and sysfs.
    
    Change-Id: Ieeff029a5010d18b50d327bc8b6e74540e63be17
    Signed-off-by: Amandeep Singh <quic_amansing@quicinc.com>

--- drivers/char/diag/diagfwd_socket.c
+++ drivers/char/diag/diagfwd_socket.c
@@ -56,6 +56,9 @@
 struct qmi_handle *cntl_qmi;
 static uint64_t bootup_req[NUM_SOCKET_SUBSYSTEMS];
 
+static unsigned long peripheral_mask = BIT(PERIPHERAL_WCNSS);
+module_param(peripheral_mask, ulong, S_IRUGO | S_IWUSR | S_IWGRP);
+
 struct diag_socket_info socket_data[NUM_PERIPHERALS] = {
 	{
 		.peripheral = PERIPHERAL_MODEM,
@@ -1078,6 +1081,9 @@ static struct diag_socket_info *diag_get_svc_sock_info(struct qmi_service *svc)
 
 	inst = svc->version | (svc->instance << 8);
 	for (i = 0; i < NUM_PERIPHERALS; i++) {
+		if (!test_bit(i, &peripheral_mask))
+			continue;
+
 		if ((svc->service == socket_cmd[i].svc_id) &&
 		    (inst == socket_cmd[i].ins_id)) {
 			info = &socket_cmd[i];
@@ -1140,6 +1146,9 @@ int diag_socket_init(void)
 	int i;
 
 	for (peripheral = 0; peripheral < NUM_PERIPHERALS; peripheral++) {
+		if (!test_bit(peripheral, &peripheral_mask))
+			continue;
+
 		info = &socket_cntl[peripheral];
 		__diag_socket_init(&socket_cntl[peripheral]);
 
@@ -1153,6 +1162,9 @@ int diag_socket_init(void)
 	}
 
 	for (i = 0; i < ARRAY_SIZE(restart_notifiers); i++) {
+		if (!test_bit(i, &peripheral_mask))
+			continue;
+
 		nb = &restart_notifiers[i];
 		rproc_register_subsys_notifier(nb->name, &nb->nb, NULL);
 		DIAG_LOG(DIAG_DEBUG_PERIPHERALS,
@@ -1170,6 +1182,9 @@ int diag_socket_init(void)
 		goto fail;
 
 	for (peripheral = 0; peripheral < NUM_PERIPHERALS; peripheral++) {
+		if (!test_bit(peripheral, &peripheral_mask))
+			continue;
+
 		info = &socket_cmd[peripheral];
 		qmi_add_lookup(cntl_qmi, info->svc_id,
 			       info->ins_id & 0xFF, info->ins_id >> 8);
@@ -1236,6 +1251,9 @@ void diag_socket_exit(void)
 		kfree(cntl_qmi);
 	}
 	for (i = 0; i < NUM_PERIPHERALS; i++) {
+		if (!test_bit(i, &peripheral_mask))
+			continue;
+
 		__diag_socket_exit(&socket_cntl[i]);
 		__diag_socket_exit(&socket_data[i]);
 		__diag_socket_exit(&socket_cmd[i]);
