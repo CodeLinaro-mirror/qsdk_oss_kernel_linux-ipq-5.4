2b67400cdad9160c7a718430b71cba72ebea19ba
Edayilliam Jayadev <quic_ejayadev@quicinc.com>
2024-05-24 14:58:00 +0530

    wifi: cfg80211: fix bss rbn double erase issue
    If cfg80211_update_assoc_bss_entry call rb_insert_bss re-insert bss
    failed because cmp_bss return 0, this bss->rbn will continue to hold
    expired data, such as __rd_parent_color.
    And this bss still in rdev->bss_list, maybe double erase in
    __cfg80211_bss_expire later.
    Double erase a rbtree node(with expired parent and color data) maybe
    corrupt rbtree, so add a in_rbtree flag to fix this issue.
    
    Link: https://patchwork.kernel.org/project/linux-wireless/patch/20231025103304.22082-1-jiazi.li@transsion.com/#25571146
    Change-Id: I91ef9ee73c88e02a4ce2ff1955cab0b5028ace34
    Signed-off-by: Edayilliam Jayadev <quic_ejayadev@quicinc.com>

--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -165,7 +165,8 @@ static bool __cfg80211_unlink_bss(struct cfg80211_registered_device *rdev,
 
 	list_del_init(&bss->list);
 	list_del_init(&bss->pub.nontrans_list);
-	rb_erase(&bss->rbn, &rdev->bss_tree);
+	if (bss->in_rbtree)
+		rb_erase(&bss->rbn, &rdev->bss_tree);
 	rdev->bss_entries--;
 	WARN_ONCE((rdev->bss_entries == 0) ^ list_empty(&rdev->bss_list),
 		  "rdev bss entries[%d]/list[empty:%d] corruption\n",
@@ -1118,6 +1119,7 @@ static void rb_insert_bss(struct cfg80211_registered_device *rdev,
 
 		if (WARN_ON(!cmp)) {
 			/* will sort of leak this BSS */
+			bss->in_rbtree = false;
 			return;
 		}
 
@@ -1127,6 +1129,7 @@ static void rb_insert_bss(struct cfg80211_registered_device *rdev,
 			p = &(*p)->rb_right;
 	}
 
+	bss->in_rbtree = true;
 	rb_link_node(&bss->rbn, parent, p);
 	rb_insert_color(&bss->rbn, &rdev->bss_tree);
 }
@@ -2227,7 +2230,8 @@ void cfg80211_update_assoc_bss_entry(struct wireless_dev *wdev,
 			rdev->bss_generation++;
 	}
 
-	rb_erase(&cbss->rbn, &rdev->bss_tree);
+	if (cbss->in_rbtree)
+		rb_erase(&cbss->rbn, &rdev->bss_tree);
 	rb_insert_bss(rdev, cbss);
 	rdev->bss_generation++;
 
@@ -2237,7 +2241,8 @@ void cfg80211_update_assoc_bss_entry(struct wireless_dev *wdev,
 		bss = container_of(nontrans_bss,
 				   struct cfg80211_internal_bss, pub);
 		bss->pub.channel = chan;
-		rb_erase(&bss->rbn, &rdev->bss_tree);
+		if (bss->in_rbtree)
+			rb_erase(&bss->rbn, &rdev->bss_tree);
 		rb_insert_bss(rdev, bss);
 		rdev->bss_generation++;
 	}
